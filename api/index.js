2edc83c677d244eecf2b628dca122ddfb553c723
Hitesh Verma <hiteshverma@Hiteshs-MacBook-Air.local>
2023-04-29 12:24:22 +0530

    vercel/api

--- api/index.js
+++ api/index.js
@@ -25,12 +25,12 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173/',
 }));
 
 async function uploadToS3(path, originalFilename, mimetype) {
   const client = new S3Client({
-    region: 'us-east-1',
+    region: 'eu-north-1',
     credentials: {
       accessKeyId: process.env.S3_ACCESS_KEY,
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
